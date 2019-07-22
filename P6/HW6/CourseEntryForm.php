c578036e495e77e8ede50944950934297715e7b6
Seraphendipity <47489930+Seraphendipity@users.noreply.github.com>
2019-07-22 17:21:26 -0500

    P6 Additional Validation

--- P6/HW6/CourseEntryForm.php
+++ P6/HW6/CourseEntryForm.php
@@ -26,7 +26,7 @@ ____ ____ ____ _  _     _ _  _ ___  _  _ ___
             <input type="text" name="lName" class="form formText" ></input>
             -->
             <label> Semester: <br>
-            <select name="semester" required>
+            <select name="semester[]" required>
                 <option value="sp">Spring</option>
                 <option value="su">Summer</option>
                 <option value="fa">Fall</option>
@@ -41,7 +41,7 @@ ____ ____ ____ _  _     _ _  _ ___  _  _ ___
             </label>
 
             <label> Days (use CTRL to select multiple):  <br>
-            <select name="days" multiple required>
+            <select name="days[]" multiple required>
                 <option value="m">Monday</option>
                 <option value="t">Tuesday</option>
                 <option value="w">Wednesday</option>
@@ -51,10 +51,10 @@ ____ ____ ____ _  _     _ _  _ ___  _  _ ___
                 <option value="u">Sunday</option>
             </select></label>
 
-            <label>Time (start -> end, hh:mm:ss, 24-hour notation):  <br>
-                <input type="time" name="timeStart" class="form teamNum" placeholder="08:00" required>
+            <label>Time (start -> end, HH:mm XM):  <br>
+                <input type="time" name="timeStart" class="form teamNum" placeholder="08:00 AM" required>
                 to
-                <input type="time" name="timeStart" class="form teamNum" placeholder="10:00" required>
+                <input type="time" name="timeEnd" class="form teamNum" placeholder="10:00 PM" required>
             </label>
 
             <label>Instructor:  <br>
@@ -87,238 +87,204 @@ ____ ____ ____ _  _     _  _ ____ _  _ ___  _    _ _  _ ____
 |    |__| |  \ |  | ___ |  | |  | | \| |__/ |___ | | \| |__] 
 ------------------------------------------------------------------------------>
 <?php 
+var_dump($_POST);
 
 //INPUT_VALIDATION - https://www.w3schools.com/php/showphp.asp?filename=demo_form_validation_complete
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $arrVals = [
-        'semester'      =>  $semester,
-        'course'        =>  $course,
-        'title'         =>  $title,
-        'days'          =>  $days,
-        'time'          =>  $time,
-        'enrolledAct'   =>  $enrolledAct,
-        'enrolledMax'   =>  $enrolledMax,
-        'instructor'    =>  $instructor,
-        'location'      =>  $location
-    ]
-    
-    //Basic Validation and Set
-    foreach ($arrVals as $name => $value) {
-        $value = validate($name);
+        'semester'      =>  [$semester = '',     'select',         [['sp', 'su', 'fa'], false]                   ],
+        'course'        =>  [$course = '',       'alphanum',       []                                        ],
+        'title'         =>  [$title = '',        'alphanum',       []                                        ],
+        'days'          =>  [$days = '',         'select',         [['m', 't', 'w', 'r', 'f', 's', 'u'], true]  ],
+        'timeStart'     =>  [$timeStart = '',    'time',           ['00:00', '23:59']                        ],
+        'timeEnd'       =>  [$timeEnd = '',      'time',           ['00:00', '23:59']                        ],
+        'enrolledAct'   =>  [$enrolledAct = '',  'num',            [0, 99999]                                ],
+        'enrolledMax'   =>  [$enrolledMax = '',  'num',            [0, 99999]                                ],
+        'instructor'    =>  [$instructor = '',   'alphanum',       []                                        ],
+        'location'      =>  [$location = '',     'alphanum',       []                                        ]  
+    ];
+
+    //Validation Functions
+    function validate_get($s, &$value) {
+        if (empty($_POST[$s])) {
+            return "ERROR - \"{$s}\": a value is required.";
+        } else {
+            $value = clean_input($_POST[$s]);
+            return 1;
+        }
     }
-    extract($arrValues);
 
-    //Semester - Only 1 of 3 options
-    $options = ['sp', 'su', 'fa'];
-    validate_select($semester, $options, false, 'semester');
+    function clean_input_base($d) {
+        $d = trim($d);
+        $d = stripslashes($d);
+        $d = htmlspecialchars($d);
+        return $d;
+    }
 
-    validate_alphanum($course);
+    function clean_input($data) {
+        if (gettype($data) == 'array') {
+            foreach ($data as $val) {
+                $val = clean_input_base($val);
+            }
+            return ($data);
+        } else {
+            return clean_input_base($data);
+        }
+    }
 
-    validate_alphanum($title);
-
-    $options = ['m', 't', 'w', 'r', 'f', 's', 'u'];
-    validate_select($days, $options, true, 'days');
-
-    validate_time($timeStart);
-    
-    validate_time($timeEnd,,$timeStart);
-
-    validate_num($enrolledAct, 0, $enrolledMax,true);
-    
-    validate_num($enrolledMax, 0,,true);
-
-    validate($instructor);
-    
-    validate($location);
-
-    function validate_time($time, $timeMin = '00:00', $timeMax = '24:00', $dtFormat = 'HH:mm') {
-        $dt = new DateTime->date_create_from_format($dtFormat, $time);
-        $dtMin = new DateTime->date_create_from_format($dtFormat, $timeMin);
-        $dtMax = new DateTime->date_create_from_format($dtFormat, $timeMax);
-
-        if ($dt == false) {
-            $errorMsg .= "<br>Invalid Datetime: Time {$time} is not of 'HH:mm' format. ";
-        } elseif ($dtMin == false) {
-            $errorMsg .= "<br>Invalid Datetime: Time Minimum {$timeMin} is not of 'HH:mm' format. ";
-        } elseif ($dtMax == false) {
-            $errorMsg .= "<br>Invalid Datetime: Time Maximum {$timeMax} is not of 'HH:mm' format. ";
+    function validate_time($time, $timeMin = '00:00 AM', $timeMax = '23:59 PM', $dtFormat = 'H:i') {
+        $dt = DateTime::createFromFormat($dtFormat, $time);
+        $dtMin = DateTime::createFromFormat($dtFormat, $timeMin);
+        $dtMax = DateTime::createFromFormat($dtFormat, $timeMax);
+        $errorMsg = '';
+        if ($dt === false) {
+            $errorMsg .= "<br>ERROR - Invalid Datetime: Time \"{$time}\" is not of 'HH:mm XM' format. ";
+        } elseif ($dtMin === false) {
+            $errorMsg .= "<br>ERROR - Invalid Datetime: Time Minimum \"{$timeMin}\" is not of 'HH:mm XM' format. ";
+        } elseif ($dtMax === false) {
+            $errorMsg .= "<br>ERROR - Invalid Datetime: Time Maximum \"{$timeMax}\" is not of 'HH:mm XM' format. ";
         } else if ($dt < $dtMin) {
-            $errorMsg .= "<br>Invalid Datetime: {$time} cannot be less than the minimum {$timeMin}. "
+            $errorMsg .= "<br>ERROR - Invalid Datetime: \"{$time}\" cannot be less than the minimum \"{$timeMin}\". ";
         } else if ($dt > $dtMax) {
-            $errorMsg .= "<br>Invalid Datetime: {$time} cannot be more than the maximum {$timeMax}. "
+            $errorMsg .= "<br>ERROR - Invalid Datetime: \"{$time}\" cannot be more than the maximum \"{$timeMax}\". ";
         } else {return 1;}
         return $errorMsg;
     }
 
     function validate_num($num, $min = '0', $max = '999999') {
+        $errorMsg = '';
         if (!is_numeric($num)) {
-            $errorMsg .= "<br>Invalid Number: {$num} is not numeric. ";
-        } else if (is_numeric($min)) {
-            $errorMsg .= "<br>Invalid Number: {$min} is not numeric. "
-        } else if (is_numeric($max)) {
-            $errorMsg .= "<br>Invalid Number: {$max} is not numeric. "
+            $errorMsg .= "<br>ERROR - Invalid Number: \"{$num}\" is not numeric. ";
+        } else if (!is_numeric($min)) {
+            $errorMsg .= "<br>ERROR - Invalid Number: \"{$min}\" is not numeric. ";
+        } else if (!is_numeric($max)) {
+            $errorMsg .= "<br>ERROR - Invalid Number: \"{$max}\" is not numeric. ";
         } else if ($num < $min) {
-            $errorMsg .= "<br>Invalid Number: {$num} cannot be less than the minimum {$min}. "
+            $errorMsg .= "<br>ERROR - Invalid Number: \"{$num}\" cannot be less than the minimum \"{$min}\". ";
         } else if ($num > $max) {
-            $errorMsg .= "<br>Invalid Number: {$num} cannot be more than the maximum {$max}. "
+            $errorMsg .= "<br>ERROR - Invalid Number: \"{$num}\" cannot be more than the maximum \"{$max}\". ";
         } else {return 1;}
         return $errorMsg;
     }
 
-    function validate_select($value, $options, $bMultiselect = false, $name = '') {
-        $bValid = false;
-        foreach ($options as $option) {
-            if ( $semester == $option ) {
-                $bValid = true;
-            }
+    function validate_select($value, $options, $bMultiselect = false) {
+        $value = array_unique($value);
+        if ( (!$bMultiselect) && (count($value) !== 1) ) {
+            return "ERROR - Selection: Multiple values in selection \"{$value}\", only one allowed.";
         }
-        if ($bValid == false) {
-            $errorMsg .= "<br>{{$name} Selection Error}: Invalid option {$semester}, requires one of the following -";
-            foreach($options as $option) {
-                $errorMsg .= " {$option} |";
+        foreach ($value as $val) {
+            $bValid = false;
+            foreach ($options as $option) {
+                if ( $val == $option ) {
+                    $bValid = true;
+                }
             }
-            return $errorMsg;
-        } else {return 1;}
+
+            if ($bValid == false) {
+                $errorMsg .= "ERROR - Selection: Invalid option \"{$semester}\", requires one of the following -";
+                foreach($options as $option) {
+                    $errorMsg .= " \"{$option}\" |";
+                }
+                return $errorMsg;
+            } 
+        }
+        if($bValid) {return 1;}
+        
     }
 
     function validate_alphanum($value) {
         if( preg_match("/^[a-zA-Z0-9 ]*$/",$value) ) {
             return 1;
         } else {
-            return "Alphanum Error: {$value} does not solely consist of numbers, letters, and whitespace."
+            return "ERROR - Alphanum: \"{$value}\" does not solely consist of numbers, letters, and whitespace.";
         }
     }
 
-
-
-
-
-
-
-
-    if( !isset($bValid) || !isset($errorMsg) ) {
-        static $bValid = true;
-        static $errorMsg = '';
-    }
-    if (empty($_POST[$s])) {
-        $errorMsg += "{$s}: a value is required. ";
-        $bValid = false;
-    } else {
-        $value = test_input($_POST[$s]);
-        // check if value only contains letters and whitespace
-        if (validate($value)) {
-            $errorMsg = "{$s}: Only letters, numbers, and white space allowed, given {$value}. "; 
-            $bValid = false;
+    //CONSIDER using call_user_func_array
+    function validate_apply($value, $method, $arrInputs = NULL) {
+        switch ($method) {
+            case 'alphanum':
+                $result = validate_alphanum($value);
+                break;
+            case 'select':
+                $result = validate_select($value, $arrInputs[0], $arrInputs[1]);
+                break;
+            case 'num':
+                $result = validate_num($value, $arrInputs[0], $arrInputs[1]);
+                break;
+            case 'time':
+                $result = validate_time($value, $arrInputs[0], $arrInputs[1]);
+                break;
+            default:
+                $result = "ERROR: Unknown function method to apply.";
         }
-    }  
-    if ($bValid) {return $value};
-  
-    function test_input($data) {
-        $data = trim($data);
-        $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-        return $data;
+        return $result;
     }
 
-    function validate($s) {
-        return preg_match("/^[a-zA-Z0-9 ]*$/",$s);
-    }
-
-    function safeGet($s) {
-        if( !isset($bValid) || !isset($errorMsg) ) {
-            static $bValid = true;
-            static $errorMsg = '';
-        }
-        if (empty($_POST[$s])) {
-            $errorMsg += "{$s}: a value is required. ";
-            $bValid = false;
-        } else {
-            $value = test_input($_POST[$s]);
-            // check if value only contains letters and whitespace
-            if (validate($value)) {
-                $errorMsg = "{$s}: Only letters, numbers, and white space allowed, given {$value}. "; 
-                $bValid = false;
-            }
-        }  
-        if ($bValid) {return $value};
-    }
-
-
-
-
-    if (!isset($_GET["BlazerID"])) {
-        $fo = fopen("../Database/project-log.csv", "r"); 
-        $nl = "\n"; $i = 0;
-        while ( (!feof($fo))) {
-            $line = fgets($fo, 200);
-            if(trim($line) == '') {
-                if ($i == 0) {
-                    echo "<p>No data to work with, sire.</p>";
-                } else {
-                    //nothing, skip
-                }
+    //Basic Validation
+    $valid;
+    foreach ($arrVals as $name => $value) {
+        $valid = validate_get($name, $value[0]);
+        if( $valid === 1 ) {
+            $valid = validate_apply($value[0], $value[1], $value[2]);
+            if( $valid === 1 ) {
+                continue;
             } else {
-            $data[$i] = str_getcsv($line, ",");
-            
-
-            //FORM HANDLING
-            $BlID       = $data[$i][0];
-            $teamNum    = $data[$i][1];
-            $time       = $data[$i][2];
-            $AnD        = $data[$i][5];
-            $code       = $data[$i][6];
-            $test       = $data[$i][7];
-            $meet       = $data[$i][8];
-            $other      = $data[$i][9];
-            $timeStamp  = $data[$i][3];
-            $dateStamp  = $data[$i][4];
-
-            echo <<<FORMRECEIPT
-                <div class="centeredBox">
-                    <h3>Project Meta Information</h3>
-                    <p>BlazerID: {$BlID}</p>
-                    <p>Team Number: {$teamNum}</p>
-                    <p>Total Time: {$time}</p>
-                    <p>Submission TimeStamp (H:M:S D-M-Y): {$timeStamp} {$dateStamp}</p>
-                    <hr/>
-                    <h3>Project Resource Composition</h3>
-                    <p>Analysis and Design: {$AnD}%</p>
-                    <p>Coding: {$code}%</p>
-                    <p>Meeting: {$meet}%</p>
-                    <p>Testing: {$test}%</p>
-                    <p>Other: {$other}%</p>
-                </div>
-FORMRECEIPT;
+                break;
             }
-            $i++;
+        } else {
+            break;
         }
-        fclose($fo);
+    }
+
+    //SQL Input
+    if( !($valid === 1) ) {
+        echo $valid;
+    } else {
+        
+    }
+}
+
 require 'db_connect.php';
 
 $conn = db_connect();
 
-$sqlcmd = "SELECT * FROM test_table";
-$result = $conn->query($sqlcmd);
-var_dump( $result );
+
+
 
 echo '<br>';
 
-$sql = $conn->prepare("INSERT INTO test_table (students) VALUES (?)");
-$sql->bind_param('s', $name);
-//$stmt->bind_param("sss", $firstname, $lastname, $email); with ? marks in statement
+$names = ''; $qmarks = ''; $types = ''; $values = ''; $bFirst = true; $i = 0;
+$sql = $conn->prepare("INSERT INTO test_table ({$names}) VALUES ({$qmarks})");
 
-$name = 'Dath Vader"; DROP students; --?';
-$sql->execute();
+foreach($arrVals as $name => $value) {
+    if (!$bFirst) {
+        $names = ', ';
+        $qmarks = ', ';
+        $values = ', ';
+    } else {$bFirst = false;}
+    $names .= $name    
+    $qmarks .= '?';
 
-// $sqlcmd = "INSERT INTO test_table (students) VALUES ('Pikachu');";
-// $result = $conn->query($sqlcmd);
+    if( gettype($value[0]) == 'integer' ) {
+        $types .= 'i';
+    } else {$types .= 's';}
+
+    if ( gettype($value[0]) == 'array' ) {
+        $values[$i] .= implode('', $value[0]);
+    } else { 
+        $values[$i] .= $value[0];
+    } $i++;
+}
+$sql->bind_param("{$types}", ...$values);
+
 // var_dump( $result );
 
 echo '<br>';
 
 echo $conn->error;
 
-db_disconnect($conn);*/
+db_disconnect($conn);
 ?>
 
 </div> </body>
